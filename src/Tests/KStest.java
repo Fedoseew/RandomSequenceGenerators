a92a055ec30c3e981bc05007fc3b92705c3e93de
Michael <al.burno@mail.ru>
2020-12-21 20:25:59 +0400

    prettier

--- src/Tests/KStest.java
+++ src/Tests/KStest.java
@@ -1,12 +1,14 @@
 package Tests;
+
 import Generators.Task2;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 
 public class KStest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ArrayList<Integer> array = (ArrayList<Integer>) Task2.generate(90, 45577);
         array.sort(Integer::compareTo);
         ArrayList<Double> newArray = new ArrayList<>();
@@ -21,17 +23,17 @@ public class KStest {
 
         int size = newArray.size();
 
-        for(int i=0;i<size;i++){
-            if((double)i/size-newArray.get(i) > maxP)
-                maxP = (double)i/size-newArray.get(i);
+        for (int i = 0; i < size; i++) {
+            if ((double) i / size - newArray.get(i) > maxP)
+                maxP = (double) i / size - newArray.get(i);
 
-            if((newArray.get(i) - ((double)((i-1)/size))) > maxM)
-                maxM = newArray.get(i)-((double)((i-1)/size));
+            if ((newArray.get(i) - ((double) ((i - 1) / size))) > maxM)
+                maxM = newArray.get(i) - ((double) ((i - 1) / size));
 
         }
-        double KnP = Math.ceil((Math.sqrt(size)*maxP) * scale)/scale;
-        double KnM = Math.ceil((Math.sqrt(size)*maxM) * scale)/scale;
-        System.out.println("Kn+ = "+KnP+", Kn- = "+KnM);
+        double KnP = Math.ceil((Math.sqrt(size) * maxP) * scale) / scale;
+        double KnM = Math.ceil((Math.sqrt(size) * maxM) * scale) / scale;
+        System.out.println("Kn+ = " + KnP + ", Kn- = " + KnM);
     }
 
 
